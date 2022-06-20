5cb1176a6e42b808e80e57bb583071d8fcff75b8
Tibebu12aa <99277608+Tibebu12aa@users.noreply.github.com>
2022-06-20 20:00:56 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
+def safe_print_list(my_list=None, x=0):
+    if my_list is None:
+        my_list = []
     count = 0
 
     for i in range(x):
@@ -11,4 +13,4 @@ def safe_print_list(my_list=[], x=0):
             count += 1
 
     print()
-    return (count)
+    return count
